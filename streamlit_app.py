db741256352dbee56c4b452d8801f6e2abaf6a98
Yao Samuel KUMA <kumasamuel94@gmail.com>
2024-08-29 07:57:22 +0200

    Initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import pandas as pd
 
-st.title('🎈 App Name')
+st.title('Machine learning app')
 
-st.write('Hello world!')
+st.info('This is a Machine learning app!')
+
+with st.expander("Data"):
+    st.write("**Raw Data**")
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+    df
